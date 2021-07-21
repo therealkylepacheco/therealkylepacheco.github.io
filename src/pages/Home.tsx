32d4cbeebdc18cc3914e6b9de76276abe7d7ba80
Kyle Pacheco <kpacheco@nnu.edu>
2021-07-21 01:59:30 -0600

    Initial text blurb

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,10 @@ export const Home = () => {
           <Content
             body="
             Hi! My name is Kyle Pacheco, and I'm a software developer in Boise, ID.
-
+            Software development is an incredibly interesting career so far. Building out new products
+            and solving complex problems while collaborating with my teammates are the highlights of my 
+            day to day. Both frontend and backend development interest me and I have experience in both.
+            Outside of work, I enjoy reading and trying new things with my friends. 
             "
             img={me}
             alt="Kyle Pacheco"
