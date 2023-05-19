599941a39a9eb267be8f24fbd315abe05236b9f8
therealkylepacheco <kpacheco@nnu.edu>
2023-05-19 11:57:01 -0600

    Setting up page

--- src/pages/projects/constants.ts
+++ src/pages/projects/constants.ts
@@ -1,9 +1,15 @@
 import { PageType } from "../types";
 import { Expander } from "./Expander";
 import { Prime } from "./Prime";
+import { ProjectSpaceShooter } from "./ProjectSpaceShooter";
 import { ThisSite } from "./ThisSite";
 
 export const Projects: PageType[] = [
+  {
+    comp: ProjectSpaceShooter,
+    title: "Space Shooter",
+    route: "/project-space-shooter",
+  },
   {
     comp: Expander,
     title: "Expander",
